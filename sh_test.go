455fa83673011162f63cfbf0c54e9ee2bcaab32b
ohkinozomu <nozomunoise@gmail.com>
2021-04-15 09:31:03 +0900

    Add cat test

---
+++ sh_test.go
@@ -0,0 +1,17 @@
+package sh
+
+import "testing"
+
+func TestCat(t *testing.T) {
+	if err := Run("echo 'test' > tmp.txt"); err != nil {
+		t.Fatal((err))
+	}
+
+	result, err := RunR("cat tmp.txt")
+	if err != nil {
+		t.Fatal((err))
+	}
+	if result != "test" {
+		t.Fatal("failed test: cat")
+	}
+}
