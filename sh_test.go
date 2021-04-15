427dc5e1e7a1c99e3c27aee2cedb514dffafa476
Nozomu Ohki <38576286+ohkinozomu@users.noreply.github.com>
2021-04-15 10:12:40 +0900

    Remove brackets (#6)

--- sh_test.go
+++ sh_test.go
@@ -4,17 +4,17 @@ import "testing"
 
 func TestCat(t *testing.T) {
 	if err := Run("echo 'test' > tmp.txt"); err != nil {
-		t.Fatal((err))
+		t.Fatal(err)
 	}
 
 	result, err := RunR("cat tmp.txt")
 	if err != nil {
-		t.Fatal((err))
+		t.Fatal(err)
 	}
 	if result != "test" {
 		t.Fatal("failed test: cat")
 	}
 	if err := Run("rm tmp.txt"); err != nil {
-		t.Fatal((err))
+		t.Fatal(err)
 	}
 }
